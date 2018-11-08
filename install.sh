bb5938cae99a3b4abbd200a88ae9fefe585d8c9c
Fabien DEL CARMEN <fdel-car@e3r3p11.42.fr>
2018-11-08 15:13:53 +0100

    Final fix for glad hopefully

--- install.sh
+++ install.sh
@@ -4,18 +4,19 @@ if [ ! -d "glfw-3.2.1" ]; then
     unzip glfw-3.2.1.zip
     rm "glfw-3.2.1.zip"
 fi
-PYTHON_VERSION="$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')"
+pythonVersion="$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')"
 which -s python || (echo "You need to have python installed first, you can use 'brew install python' for instance." && exit)
 which -s pip
 if [ $? == 1 ]; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python get-pip.py --user
     rm get-pip.py
-    echo -e "\033[0;33mNow that pip is installed please note that you should add it to your path.\n'export PATH=~/Library/Python/${PYTHON_VERSION}/bin:\$PATH' in your .bashrc could do the trick.\033[0m"
+    echo -e "\033[0;33mNow that pip is installed please note that you should add it to your path.\n\
+    So the commands pip and glad won't work for you as it, but that line: 'export PATH=~/Library/Python/${pythonVersion}/bin:\$PATH' in your .bashrc will do the trick.\033[0m"
 fi
-which -s glad || (~/Library/Python/${PYTHON_VERSION}/bin/pip install glad --user)
+which -s glad || (~/Library/Python/${pythonVersion}/bin/pip install glad --user)
 if [[ ! -d "srcs/glad" && ! -d "includes/glad" && ! -d "includes/KHR" ]]; then
-    glad --api gl=4.1 --profile=core --generator=c --out-path=tmp-glad
+    ~/Library/Python/${pythonVersion}/bin/glad --api gl=4.1 --profile=core --generator=c --out-path=tmp-glad
     mkdir srcs/glad && mv tmp-glad/src/glad.c srcs/glad/
     mv tmp-glad/include/KHR includes/ && mv tmp-glad/include/glad includes/
     rm -rf tmp-glad
